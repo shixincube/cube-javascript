b351d09980fd9dfef9c986cff6ac03bb464ac856
Ambrose Xu <xujiangwei@spap.com>
2020-09-30 14:42:05 +0800

    Add web app sign-in view

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Cube - 时信魔方' });
+    let cookie = req.cookies['AppToken'];
+    if (cookie) {
+        res.render('index', { title: 'Cube - 时信魔方' });
+    }
+    else {
+        
+        res.render('signin', { title: '登录 - Cube Web Application' });
+    }
 });
 
 module.exports = router;
