5f84c215cdd734325c332b4ea3164346b07e94fa
Ambrose Xu <xujiangwei@spap.com>
2020-10-02 14:19:45 +0800

    Update sign in form

--- web/app.js
+++ web/app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -17,6 +18,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- web/routes/account.js
+++ web/routes/account.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
 
+/* POST signin/form */
+router.post('/signin/form', function(req, res, next) {
+    console.log(req.body.id);
+});
+
 module.exports = router;
