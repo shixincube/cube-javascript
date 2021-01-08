3fb59590bec7c38ee77bd266bc8d819fb47363dd
Ambrose Xu <xujiangwei@spap.com>
2021-01-08 16:25:16 +0800

    Update media device tool

--- cube/src/util/MediaDeviceTool.js
+++ cube/src/util/MediaDeviceTool.js
@@ -3,7 +3,7 @@
  * 
  * The MIT License (MIT)
  *
- * Copyright (c) 2020-2021 Shixin Cube Team.
+ * Copyright (c) 2020 Shixin Cube Team.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,7 +24,14 @@
  * SOFTWARE.
  */
 
- section#contactSetting div {
-    display: inline-block;
-    margin-right: 2em;
+/**
+ * 媒体设备工具。
+ */
+export class MediaDeviceTool {
+
+    constructor() {
+
+    }
+
+    
 }
